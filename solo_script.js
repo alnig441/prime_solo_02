98e7cb90d377f954164a0dc25941e0c5576b31b4
alnig441 <alnig441@gmail.com>
2015-08-16 16:56:44 -0500

    inital commit, no random emp generator, no salary total

--- solo_script.js
+++ solo_script.js
@@ -1,100 +1,125 @@
-// ! ! !
-// Three Bugs
-// Line 23: calculateSTI(array) was not called with the corect array index. 
-// Line 72: return statement incorrect, -1 removed.
-// Line 46/47: rounding applied.
+var decSal = [];
+var totalSal = 0;
+var empArray = [];  
+
+  var Employee = function(first, last, number, title, rating, salary){
+  this.firstName = first;
+  this.lastName = last;
+  this.employeeNumber = number;
+  this.jobTitle = title;
+  this.reviewScore = rating;
+  this.salary = salary;
+  totalSal += parseInt(this.salary);
+  };
+
+$(document).ready(function(){
+
+   $('#submit').on('click', function(e){
+
+    console.log(decSal);
+    
+    //Collect values from form fields
+    var firstName = $('#firstname').val();
+    var lastName = $('#lastname').val();
+    var empNum = $('#id').val();
+    var title = $('#title').val();
+    var rating = $('#rating').val();
+    var salary = $('#salary').val();
+    var newEmployee = new Employee(firstName, lastName, empNum, title, rating, salary)
+    var decSal = [];
+    //Build array of emloyees
+    empArray.push(newEmployee);
+
+    //Populate DOM with employee elements
+    postData(newEmployee);
+
+    $('ul button').text('Remove');
+    $('ul button').addClass('remove');
+
+    //Remove employee from list
+    $('.remove').on('click', function(){
+      decSal = parseInt((decrement(this).toString()));
+      // decsal = parseInt(decSal.toString())
+      // totalSal = totalSal - decSal;
+      // console.log(totalSal);
+      $(this).parent().remove();
+    })
 
 
-var arrayAtticus = ["Atticus", "2405", "47000", 3];
-var arrayJem = ["Jem", "62347", "63500", 4];
-var arrayBoo = ["Boo", "11435", "54000", 3];
-var arrayScout = ["Scout", "6243", "74750", 5];
+    //Sort function on click 
+    $('#sort').on('click', function(){
+      $('#content').children().remove();  
+      empArray.sort(sortOn("lastName"));
+      for(var i = 0; i<empArray.length; i++){
+        postData(empArray[i]);
+        $('ul button').text('Remove');
+        $('ul button').addClass('remove');
+      }
+      $('.remove').on('click', function(){
+        decrement(this);
+        $(this).parent().remove();
+      })
+    })
+      console.log(empArray);
+      e.preventDefault();
+    })
 
-var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+    
+  })
 
-//Create variables used to write to the DOM
-var newEl, newText, position;
-//Capture the position of insertion into the DOM
-position = document.getElementById('content');
+function postData(employee){
 
-//Loop the array, extracting each array and writing information to the DOM
-//Note that the information is not 'clean'
-for(var i = 0; i < array.length; i++){
-  // console.log(typeof array);
-	array[i] = calculateSTI(array[i]);  
-  // console.log(array[i]);
- 	newEl = document.createElement('li');
-  // console.log(newEl);
-	newText = document.createTextNode(array[i]);
-  // console.log(newText.toString());
-  // console.log>(newText);
-	newEl.appendChild(newText);
-	position.appendChild(newEl);
+  position = document.getElementById('content');
+    // empCount++;
+    // totalSal += parseInt(employee['salary']);
+
+    var string = "";
+ 
+    // for(var i = 0; i<employee.length; i++){
+      for(prop in employee){
+        string += " " + [prop] + ": " + employee[prop].toUpperCase() + " ";
+      }
+
+      newEl = document.createElement('li');
+      // newEl.setAttribute('value', employee['salary']);
+
+      if(employee['reviewScore']==1){
+        newEl.className="danger";
+      }
+    
+
+    newBut = document.createElement('button');
+    // newBut.className='remove';
+    newBut.setAttribute('value', employee['salary']);
+
+
+    newText = document.createTextNode(string);
+    newEl.appendChild(newBut);
+    newEl.appendChild(newText);
+    position.appendChild(newEl);
+  
 }
 
-function calculateSTI(array){
-  var newArray = [];
-
-  newArray[0] = ' ' + array[0];
-
-  var employeeNumber = array[1];
-  var baseSalary = array[2];
-  var reviewScore = array[3];
-
-  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
-  // console.log(bonus);
-  if(bonus > 0.13){
-    bonus = 0.13;
-  }
-
-  newArray[1] = '\t' + bonus ;
-  newArray[2] = '\t' + Math.round(baseSalary * (1.0 + bonus)); // rounding applied
-  newArray[3] = '\t' + Math.round(baseSalary * bonus);   // rounding applied
-  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
-  newArray = newArray.toString();
-  // console.log(newArray);
-  return newArray;
+function decrement(element){
+    var temp = [];
+    temp.push(parseInt(element.getAttribute('value')));    
+    return temp;
 }
 
-function getBaseSTI(reviewScore){
-  var basePercent;
-  switch(reviewScore){
-    case 1:
-      basePercent = 0;
-      break;
-    case 2:
-      basePercent = 0;
-      break;
-    case 3:
-      basePercent = 0.04;
-      break;
-    case 4:
-      basePercent = 0.06;
-      break;
-    case 5:
-      basePercent = 0.10;
-      break;
-  }
-  // console.log(basePercent);
-  return basePercent; // removed -1 from the return statement
+function sortOn(property){
+    return function(a, b){
+        if(a[property] < b[property]){
+            return -1;
+        }else if(a[property] > b[property]){
+            return 1;
+        }else{
+            return 0;   
+        }
+    }
 }
 
-function getYearAdjustment(employeeNumber){
-  // console.log(employeeNumber);
-  var yearAdjustment = 0;
-  if(employeeNumber.length == 4){
-    yearAdjustment = 0.05;
-  }
-  // console.log(yearAdjustment);
-  return yearAdjustment;
-}
 
-function getIncomeAdjustment(salary){
-  var incomeAdjustment = 0;
-  salary = parseInt(salary);
-  if(salary > 65000){
-    incomeAdjustment = 0.01;
-  }
-  // console.log(incomeAdjustment);
-  return incomeAdjustment;
-}
+
+
+
+
